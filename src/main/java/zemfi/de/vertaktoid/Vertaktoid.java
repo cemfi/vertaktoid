304776e7ca6f0d145d03b78284b4a74cb248d807
Yevgen Mexin <yevgen@mail.uni-paderborn.de>
2016-12-12 21:47:51 +0100

    missing file

---
+++ src/main/java/zemfi/de/vertaktoid/Vertaktoid.java
@@ -0,0 +1,13 @@
+package zemfi.de.vertaktoid;
+
+import android.app.Application;
+
+/**
+ * Created by yevgen on 12.12.2016.
+ */
+
+public class Vertaktoid extends Application {
+    public void onCreate() {
+        super.onCreate();
+    }
+}
