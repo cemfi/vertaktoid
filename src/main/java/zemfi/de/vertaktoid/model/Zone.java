67030a4e8517551be0ce6ad6f32fc73dccfabb2d
yevgen <eugene.mexin@gmail.com>
2017-04-07 09:50:27 +0200

    minimal bounding box algo

--- src/main/java/zemfi/de/vertaktoid/model/Zone.java
+++ src/main/java/zemfi/de/vertaktoid/model/Zone.java
@@ -1,10 +1,14 @@
 package zemfi.de.vertaktoid.model;
 
 
+import android.graphics.PointF;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+
+import zemfi.de.vertaktoid.helpers.RotatingCalipers;
 
 public class Zone implements Comparable<Zone>, Serializable {
     public String zoneUuid = null;
@@ -36,7 +40,15 @@ public class Zone implements Comparable<Zone>, Serializable {
     }
 
     public void convertToExtended() {
-        // TODO implement
+        List<PointF> verticesPF = new ArrayList<>();
+        for(float[] vertex: vertices) {
+            verticesPF.add(new PointF(vertex[0], vertex[1]));
+        }
+        PointF[] minBoundingBox = RotatingCalipers.getMinimumBoundingRectangle(verticesPF);
+        vertices.clear();
+        for(PointF point: minBoundingBox) {
+            vertices.add(new float[]{point.x, point.y});
+        }
     }
 
     public void convertToPolygonal() {
